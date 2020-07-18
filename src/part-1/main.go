1c963c737e2557a5b699b3e622a27b5da17203c8
wenzj-code <wzjking0929@163.com>
2020-07-18 14:02:19 +0800

    none

--- src/part-1/main.go
+++ src/part-1/main.go
@@ -14,13 +14,13 @@ import (
 
 /**
  * @description: main funtion
- * @param {type}   
+ * @param {type}
  * @return:
  */
 func main() {
 	bc := NewBlockChain()
 
-	bc.AddBlock("Send 1 BTC to Ivan")
+	bc.AddBlock("Send 1 BTC to Ivan dd")
 	bc.AddBlock("Send 2 more BTC to Ivan")
 
 	for _, block := range bc.blocks {
